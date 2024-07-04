a16da799258ae284862465aa58032645b4a6a0f9
parakh3 <parakhagarwal945@gmail.com>
2024-07-04 13:17:40 +0530

    purchase item updated

--- src/views/admin/purchaseitems/components/PurchaseitemTable.jsx
+++ src/views/admin/purchaseitems/components/PurchaseitemTable.jsx
@@ -27,7 +27,7 @@ const PurchaseitemTable = () => {
             <div class="w-1/2">
               <label for="item-weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item Name</label>
               <input type="number" name="item-weight" id="item-weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Item Name" required="" />
-              <span class="text-sm text-red-500" id="item-name-error">Item Name is required</span>
+              {/* <span class="text-sm text-red-500" id="item-name-error">Item Name is required</span> */}
 
               <div class="w-full mt-4">
                 <label for="item-weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Purpose</label>
